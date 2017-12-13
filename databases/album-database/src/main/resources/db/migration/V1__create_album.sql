451b0e2d5e96b1fc3148d6439460200864ebd44f
Meerkat5 <meerkat5@pal.com>
2017-12-13 14:02:29 -0500

    Migrations

---
+++ databases/album-database/src/main/resources/db/migration/V1__create_album.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS album;
+CREATE TABLE album
+(
+  id     BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(255),
+  rating INT(11)                NOT NULL,
+  title  VARCHAR(255),
+  year   INT(11)                NOT NULL
+);
